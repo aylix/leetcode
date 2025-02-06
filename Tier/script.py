dd0793b71d6de21e08a8effa7ee18d38a2dee7d8
aylix <54117566+aylix@users.noreply.github.com>
2025-02-06 17:25:56 +0000

    edited

--- Tier/script.py
+++ Tier/script.py
@@ -28,37 +28,34 @@ class Tier:
             current_node=childnode
         
 
-    def count_words_under_node(self,node :Tiernode,count):
+
+
+    def count_words_under_node(self,node :Tiernode,lst):
         
-        if len(node.children)==0:
-            return count
-        c=count+np.count_nonzero(np.array([node.children[child].is_end_of_word for child in node.children]))
-        for ch in node.children:
-            #count=count+np.count_nonzero(np.array([node.children[child].is_end_of_word for child in node.children]))
-            c=c+self.count_words_under_node(node.children[ch],count)
-            
-        return c
-    
-
-
+        if(node.is_end_of_word and  node not in lst):
+            lst.append(node)
+        for child in node.children:
+            if node.children[child].is_end_of_word and node.children[child] not in lst:
+                lst.append(node.children[child])
+            self.count_words_under_node(node.children[child],lst)
+ 
+        return len(lst)
+        
 
 
     def statrs_with(self,partial):
         current_node=self.root
-        count=0
         for i in range(0,len(partial)):
-            children=current_node.get_children_names() 
-            if  partial[i] in children : 
-                count=count+np.count_nonzero(np.array([current_node.children[ch].is_end_of_word for ch in children]))
-                # move current node to the child  repesenting next char
+            children_names=current_node.get_children_names()
+            #print(children_names)
+            if partial[i] in children_names:
                 current_node=current_node.children[partial[i]]
-            # retun 0 where not all of the partial exists
             else:
                 return 0
+        return self.count_words_under_node(current_node,[])
             
-
-
-        return self.count_words_under_node(current_node,count)
+            
+            
 
 
 
@@ -66,10 +63,9 @@ class Tier:
 
 root=Tiernode()
 tier=Tier(root)
-tier.insert("car")
-tier.insert("cara")
-tier.insert("cat")
-tier.insert("aylar")
+tier.insert("hack")
+tier.insert("hackerrank")
+
 #tier.print(root)
-print(tier.statrs_with("car"))
+print(tier.statrs_with("hak"))
 #print(tier.count_leave(root))
